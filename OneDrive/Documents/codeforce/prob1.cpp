4585173d3f234b653428b9bc565e005590962421
Divy <divy92288@gmail.com>
2026-07-13 22:29:18 +0530

    Add initial solution for prob1

---
+++ OneDrive/Documents/codeforce/prob1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int div;
+    cin>>n;
+    if(n%2==0)
+    {
+        if(n==2)
+        {
+            cout<<"NO";
+        }
+        else
+        {
+            cout<<"YES";
+        }
+    }
+    else
+    {
+        cout<<"NO";
+    }
+    
+}
